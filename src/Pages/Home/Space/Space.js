42d8227d66874a7835380bd3a4972809308d16d0
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-05-06 22:35:12 +0600

    creae repo or add header or simple navbar

---
+++ src/Pages/Home/Space/Space.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Space = () => {
+    return (
+        <div>
+            <h1>this is space</h1>
+        </div>
+    );
+};
+
+export default Space;
---
+++ src/Pages/Sheared/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+const Header = () => {
+    const imgUrl  = 'https://media.istockphoto.com/photos/star-sky-at-night-space-background-picture-id841403680?b=1&k=20&m=841403680&s=170667a&w=0&h=XjTDHhmVPGUxqYqGEClDlLR4otM9mP7uPl3sbeNtEvA='
+    return (
+        <div className=' py-2 text-white font-semibold text-xl' style={{
+            backgroundImage: `url(${imgUrl})`
+        }}>
+            <div className='flex justify-between w-3/4 mx-auto'>
+                <div>
+        <FontAwesomeIcon icon={faCoffee}/>
+                </div>
+                <div>
+                    <Link to='/space'>Space</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
