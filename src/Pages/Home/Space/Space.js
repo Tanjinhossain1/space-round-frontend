319a3e782a1d4d77dd37520ef07d74ea0ceb6533
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-05-07 00:11:54 +0600

    space page is done

--- src/Pages/Home/Space/Space.js
+++ src/Pages/Home/Space/Space.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
 const Space = () => {
+    const imgUrl = 'https://i.ibb.co/KKqLZnx/space-3.jpg'
     return (
         <div>
-            <h1>this is space</h1>
+            {/* h-[691px] */}
+
+            <div style={{
+                backgroundImage: `url(${imgUrl})`
+            }} className=' text-white bg-no-repeat '>
+                <div className='flex py-40  justify-around'>
+                    <div className='w-1/4 mt-12'>
+                        <h1 className='text-8xl'>SPACE</h1>
+                        <p>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</p>
+                    </div>
+                    <div>
+                        <button className='border bg-white text-black text-2xl px-28 py-36 rounded-[700px] '>EXPLORE</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
